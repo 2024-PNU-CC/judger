ce62b10c5c880c9811d4c0a37505175a0e40ca67
Jeong-Yunseo <216yoon@gmail.com>
2024-05-24 02:11:55 +0900

    원격 mysql 연결

--- myproject/my_settings.py
+++ myproject/my_settings.py
@@ -1,10 +1,15 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myproject',
-        'USER': 'myproject_user',
-        'PASSWORD': 'password1234',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        # 'NAME': 'myproject',
+        # 'USER': 'myproject_user',
+        # 'PASSWORD': 'password1234',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '3306',
+        'NAME': 'cc_schema',
+        'USER': 'MYSQL_USER_EXAMPLE',
+        'PASSWORD': 'MYSQL_PASSWORD_EXAMPLE',
+        'HOST': '119.69.22.170',
+        'PORT': '19286',
     }
 }
