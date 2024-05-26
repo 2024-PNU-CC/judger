99e84a47e8f827630bac4795e6ebd145bda2a0b3
Jeong-Yunseo <216yoon@gmail.com>
2024-05-27 01:07:32 +0900

    delete myproject

--- myproject/submissions/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import submit_code, success, send_code
-
-urlpatterns = [
-    path('', submit_code, name='submit_code'),
-    path('success/', success, name='success'),
-    path('send_code/', send_code, name='send_code')
-]
